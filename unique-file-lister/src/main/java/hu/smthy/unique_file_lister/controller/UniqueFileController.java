39b21dbc3620dac3eb8cde3f5e1ed62ea1bb64a1
Kovács Áron <kovacs.aron13@gmail.com>
2025-05-10 00:48:50 +0200

    added @RestController and TODO

---
+++ unique-file-lister/src/main/java/hu/smthy/unique_file_lister/controller/UniqueFileController.java
@@ -0,0 +1,10 @@
+package hu.smthy.unique_file_lister.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UniqueFileController {
+    
+    // TODO: add endpoints
+
+}
