1408a68e7f6604db8d6b0c773642ad0836f7646d
Kovács Áron <kovacs.aron13@gmail.com>
2025-05-09 13:59:28 +0200

    init spring boot project

---
+++ unique-file-lister/src/main/java/hu/smthy/unique_file_lister/UniqueFileListerApplication.java
@@ -0,0 +1,13 @@
+package hu.smthy.unique_file_lister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniqueFileListerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniqueFileListerApplication.class, args);
+	}
+
+}
---
+++ unique-file-lister/src/test/java/hu/smthy/unique_file_lister/UniqueFileListerApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.smthy.unique_file_lister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniqueFileListerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
