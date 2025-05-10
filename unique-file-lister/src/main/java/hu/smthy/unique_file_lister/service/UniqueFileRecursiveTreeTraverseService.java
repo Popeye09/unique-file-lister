4dfb854699873c9c8fcaeef658a1a4bd47c60666
Kovács Áron <kovacs.aron13@gmail.com>
2025-05-10 23:03:26 +0200

    Implement recursive tree search, added exceptions

---
+++ unique-file-lister/src/main/java/hu/smthy/unique_file_lister/service/UniqueFileRecursiveTreeTraverseService.java
@@ -0,0 +1,62 @@
+package hu.smthy.unique_file_lister.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.NotDirectoryException;
+import java.nio.file.Path;
+import java.util.Map;
+import java.util.HashMap;
+
+@Service
+public class UniqueFileRecursiveTreeTraverseService implements UniqueFileService{
+
+    private final FileReaderService fileReaderService;
+
+    @Autowired
+    public UniqueFileRecursiveTreeTraverseService(FileReaderService fileReaderService) {
+        this.fileReaderService = fileReaderService;
+    }
+
+    public Map<String, Integer> uniqueFiles(Path path) throws FileNotFoundException, NotDirectoryException, SecurityException{
+        File file = path.toFile();
+
+
+        if(!fileReaderService.exists(file)) {
+            throw new FileNotFoundException("Path must lead to an existing directory");
+        }
+        if(!fileReaderService.isDirectory(file)){
+            throw new NotDirectoryException("Path must lead to a directory");
+        }
+        if(!fileReaderService.canRead(file)){
+            throw new SecurityException("Unable to read directory contents: " + file.getName() + " has no read permission for " + System.getProperty("user.name"));
+        }
+
+        Map<String, Integer> map = new HashMap<>();
+
+        recursiveTreeSearch(path, map);
+        
+        return map;
+    }
+
+    void recursiveTreeSearch(Path path, Map<String, Integer> map){
+
+        File[] files;
+        try{
+            files = fileReaderService.listFiles(path.toFile());
+        } catch (SecurityException e) {
+            return;
+        }
+
+        for (File file : files) {
+            if (fileReaderService.isFile(file)) {
+                map.put(file.getName(), map.getOrDefault(file.getName(), 0) + 1);
+            }
+            if (fileReaderService.isDirectory(file)) {
+                recursiveTreeSearch(file.toPath(), map);
+            }
+        }
+    }
+}
