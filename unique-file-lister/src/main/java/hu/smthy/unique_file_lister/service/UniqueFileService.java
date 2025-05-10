31df62b91c5a445a22990b308bb98eb44d831541
Kovács Áron <kovacs.aron13@gmail.com>
2025-05-10 23:04:21 +0200

    updated interface method signature

--- unique-file-lister/src/main/java/hu/smthy/unique_file_lister/service/UniqueFileService.java
+++ unique-file-lister/src/main/java/hu/smthy/unique_file_lister/service/UniqueFileService.java
@@ -1,7 +1,10 @@
 package hu.smthy.unique_file_lister.service;
 
+import java.io.FileNotFoundException;
+import java.nio.file.NotDirectoryException;
+import java.nio.file.Path;
 import java.util.Map;
 
 public interface UniqueFileService {
-    public Map<String, Integer> uniqueFiles(String path);
+    public Map<String, Integer> uniqueFiles(Path path) throws FileNotFoundException, NotDirectoryException, SecurityException;
 }
