8b9b4e6d1cbc5d617b369daa0a4e50652c1107ba
Kovács Áron <kovacs.aron13@gmail.com>
2025-05-11 15:23:02 +0200

    added lombok for data class management

--- unique-file-lister/src/main/java/hu/smthy/unique_file_lister/domain/UniqueFileAccess.java
+++ unique-file-lister/src/main/java/hu/smthy/unique_file_lister/domain/UniqueFileAccess.java
@@ -2,7 +2,15 @@ package hu.smthy.unique_file_lister.domain;
 
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Entity
 @Table(name = "history")
 public class UniqueFileAccess {
@@ -13,20 +21,8 @@ public class UniqueFileAccess {
             generator = "unique_file_access_id_sequence"
     )
     private Long id;
-    private String user;
+    private String username;
     private Long timestamp;
     private String directory;
     private String extension;
-
-    public UniqueFileAccess(Long id, String user, Long timestamp, String directory, String extension){
-        this.id = id;
-        this.user = user;
-        this.timestamp = timestamp;
-        this.directory = directory;
-        this.extension = extension;
-    }
-
-    public Long getId() {
-        return id;
-    }
 }
