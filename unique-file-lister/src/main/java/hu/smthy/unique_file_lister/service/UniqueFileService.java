74b61d7de0efb605c641357b4b3d79c6342fee75
Kovács Áron <kovacs.aron13@gmail.com>
2025-05-11 23:16:39 +0200

    added setfilefilter to UniqueFileService interface

--- unique-file-lister/src/main/java/hu/smthy/unique_file_lister/service/UniqueFileService.java
+++ unique-file-lister/src/main/java/hu/smthy/unique_file_lister/service/UniqueFileService.java
@@ -1,5 +1,6 @@
 package hu.smthy.unique_file_lister.service;
 
+import java.io.FileFilter;
 import java.io.FileNotFoundException;
 import java.nio.file.NotDirectoryException;
 import java.nio.file.Path;
@@ -8,4 +9,6 @@ import java.util.Map;
 public interface UniqueFileService {
     Map<String, Integer> getUniqueFiles(String path, String username)
             throws FileNotFoundException, NotDirectoryException, SecurityException;
+
+    void setFileFilter(FileFilter fileFilter);
 }
