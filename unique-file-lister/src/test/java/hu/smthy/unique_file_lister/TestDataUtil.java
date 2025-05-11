ae8855f9ae5df748552b95aeff76e68b944a48f1
Kovács Áron <kovacs.aron13@gmail.com>
2025-05-11 16:04:01 +0200

    added randomUniqueFileAccess creation

--- unique-file-lister/src/test/java/hu/smthy/unique_file_lister/TestDataUtil.java
+++ unique-file-lister/src/test/java/hu/smthy/unique_file_lister/TestDataUtil.java
@@ -2,7 +2,10 @@ package hu.smthy.unique_file_lister;
 
 import hu.smthy.unique_file_lister.domain.UniqueFileAccess;
 
+import java.util.Random;
+
 public class TestDataUtil {
+    private static final Random random = new Random();
     private TestDataUtil(){
     }
 
@@ -10,6 +13,15 @@ public class TestDataUtil {
         return UniqueFileAccess.builder()
                 .username("root")
                 .timestamp(System.currentTimeMillis())
+                .directory("/")
+                .extension(".txt")
+                .build();
+    }
+
+    public static UniqueFileAccess createRandomUniqueFileAccess(){
+        return UniqueFileAccess.builder()
+                .username("root")
+                .timestamp(random.nextLong(1746971812))
                 .directory("/")
                 .extension(".txt")
                 .build();
