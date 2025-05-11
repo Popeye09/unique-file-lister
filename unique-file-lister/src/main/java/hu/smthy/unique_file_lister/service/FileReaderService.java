1d0b45fa540d494cbae74a07bb0798bee4b449fb
Kovács Áron <kovacs.aron13@gmail.com>
2025-05-12 01:16:52 +0200

    added listFile to FileReaderService interface with one argument

--- unique-file-lister/src/main/java/hu/smthy/unique_file_lister/service/FileReaderService.java
+++ unique-file-lister/src/main/java/hu/smthy/unique_file_lister/service/FileReaderService.java
@@ -9,4 +9,5 @@ public interface FileReaderService {
     boolean isFile(File file);
     boolean canRead(File file);
     File[] listFiles(File file, FileFilter fileFilter) throws SecurityException;
+    File[] listFiles(File file) throws SecurityException;
 }
