ff3189af3bc02c09f1abe5b3da2807c71526d042
Kovács Áron <kovacs.aron13@gmail.com>
2025-05-11 23:16:15 +0200

    added save method to historyservice interface

--- unique-file-lister/src/main/java/hu/smthy/unique_file_lister/service/HistoryService.java
+++ unique-file-lister/src/main/java/hu/smthy/unique_file_lister/service/HistoryService.java
@@ -2,8 +2,7 @@ package hu.smthy.unique_file_lister.service;
 
 import hu.smthy.unique_file_lister.domain.UniqueFileAccess;
 
-import java.util.List;
-
 public interface HistoryService {
-    List<UniqueFileAccess> readAll();
+    Iterable<UniqueFileAccess> readAll();
+    void save(UniqueFileAccess uniqueFileAccess);
 }
