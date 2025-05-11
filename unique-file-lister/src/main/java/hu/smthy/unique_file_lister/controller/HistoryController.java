c447233842e9246b2706e9852ec51818e1f3d978
Kovács Áron <kovacs.aron13@gmail.com>
2025-05-11 23:17:06 +0200

    implement history controller

--- unique-file-lister/src/main/java/hu/smthy/unique_file_lister/controller/HistoryController.java
+++ unique-file-lister/src/main/java/hu/smthy/unique_file_lister/controller/HistoryController.java
@@ -1,10 +1,21 @@
 package hu.smthy.unique_file_lister.controller;
 
+import hu.smthy.unique_file_lister.domain.UniqueFileAccess;
+import hu.smthy.unique_file_lister.service.HistoryService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HistoryController {
+    private final HistoryService historyService;
 
-    // TODO: implement history endpoint
+    public HistoryController(HistoryService historyService) {
+        this.historyService = historyService;
+    }
+
+    @GetMapping(path = "/history", produces = "application/json")
+    public Iterable<UniqueFileAccess> history(){
+        return historyService.readAll();
+    }
 
 }
