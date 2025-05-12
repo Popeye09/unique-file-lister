6b4254759fcd67b9846375d3c15dbc85a3cd42ae
Kovács Áron <kovacs.aron13@gmail.com>
2025-05-12 12:29:39 +0200

    Added openapi bean configuration

---
+++ unique-file-lister/src/main/java/hu/smthy/unique_file_lister/configuration/OpenApiConfiguration.java
@@ -0,0 +1,26 @@
+package hu.smthy.unique_file_lister.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfiguration {
+    @Bean
+    public OpenAPI uniqueFileListerAPIDescription() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Unique File Lister API")
+                        .version("1.0.0")
+                        .description("Recursively traverse the directory and find files with unique base name." +
+                                " Return a JSON array of file base names with number of occurrences." +
+                                " Note that the implementation does not check for symbolic links, so they are also found.")
+                        .contact(new Contact()
+                                .name("Kovács Áron")
+                                .email("kovacs.aron13@gmail.com")
+                        )
+                );
+    }
+}
