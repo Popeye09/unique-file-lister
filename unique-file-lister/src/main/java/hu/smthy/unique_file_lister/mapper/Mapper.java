8ddafdbf9f9ec39a9a316771ee9c42bd4b64d0c0
Kovács Áron <kovacs.aron13@gmail.com>
2025-05-12 15:33:29 +0200

    added javado to mappers

--- unique-file-lister/src/main/java/hu/smthy/unique_file_lister/mapper/Mapper.java
+++ unique-file-lister/src/main/java/hu/smthy/unique_file_lister/mapper/Mapper.java
@@ -1,5 +1,10 @@
 package hu.smthy.unique_file_lister.mapper;
 
+/**
+ * Interface for mapping objects
+ * @param <T> Class
+ * @param <F> Class
+ */
 public interface Mapper<T, F> {
     F mapTo(T t);
     T mapFrom(F f);
--- unique-file-lister/src/main/java/hu/smthy/unique_file_lister/mapper/UniqueFileAccessMapper.java
+++ unique-file-lister/src/main/java/hu/smthy/unique_file_lister/mapper/UniqueFileAccessMapper.java
@@ -5,6 +5,9 @@ import hu.smthy.unique_file_lister.domain.UniqueFileAccess;
 import hu.smthy.unique_file_lister.domain.UniqueFileAccessData;
 import org.springframework.stereotype.Component;
 
+/**
+ * Component implementation for mapping between {@link UniqueFileAccess} service level and {@link UniqueFileAccessData} repository objects.
+ */
 @Component
 public class UniqueFileAccessMapper implements Mapper<UniqueFileAccessData, UniqueFileAccess> {
 
@@ -14,10 +17,20 @@ public class UniqueFileAccessMapper implements Mapper<UniqueFileAccessData, Uniq
         this.modelMapper = modelMapper;
     }
 
+    /**
+     * Maps a {@link UniqueFileAccessData} object to {@link UniqueFileAccess}
+     * @param uniqueFileAccessData object to map from
+     * @return mapped {@link UniqueFileAccess} object
+     */
     public UniqueFileAccess mapTo(UniqueFileAccessData uniqueFileAccessData) {
         return modelMapper.map(uniqueFileAccessData, UniqueFileAccess.class);
     }
 
+    /**
+     * Maps a {@link UniqueFileAccess} object to {@link UniqueFileAccessData}
+     * @param uniqueFileAccess object to map from
+     * @return mapped {@link UniqueFileAccessData} object
+     */
     public UniqueFileAccessData mapFrom(UniqueFileAccess uniqueFileAccess) {
         return modelMapper.map(uniqueFileAccess, UniqueFileAccessData.class);
     }
